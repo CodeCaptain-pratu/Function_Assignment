920512ed65149e3483a38d9b4a555973019efbde
pratiksha <pratu@gmail.com>
2024-12-25 23:56:24 +0530

    QUESTION 2 completed

---
+++ QUESTION2/INPUT/script.js
@@ -0,0 +1,9 @@
+function generateGreeting(name){
+    return `Hello ${name} Welcome to our home`;
+}
+const person1 = generateGreeting('John');
+console.log(person1);
+const person2 = generateGreeting('Alice');
+console.log(person2);
+const person3 = generateGreeting('Bob');
+console.log(person3);
